a63db2803f25c8ecfa6a5603df353a2ed232a8da
Jakub Kryštůfek <krystufekjakub@seznam.cz>
2023-02-28 19:11:17 +0100

    Slightly better, but needs optimalization

--- src/KnapsackSolver.hs
+++ src/KnapsackSolver.hs
@@ -18,9 +18,9 @@ getVariantWeightAndCost [] [] = (0, 0)
 getVariantWeightAndCost (item : items) (isItemIncluded : variants) =
     -- If item is included in this variant add his cost and weight to sum
     if isItemIncluded == 1
-        then do 
+        then do
             let (weightSum, costSum) = getVariantWeightAndCost items variants
-            (weight item + weightSum , cost item + costSum) 
+            (weight item + weightSum , cost item + costSum)
         else getVariantWeightAndCost items variants
 
 -- Returns all permutations of 0 and 1 (generatePermutations 2 = [[0,0], [1,0], [0,1], [1,1]])
@@ -31,20 +31,22 @@ generatePermutations n = [ x:xs | x <- [1,0], xs <- generatePermutations (n-1) ]
 
 -- Returns 
 -- Params: Knapsack -> All variants -> Best variant so far -> returns best variant
-getBestVariant :: Knapsack -> [[Int]] -> (Int , [Int]) -> (Int , [Int])
-getBestVariant _ [] bestVariant = bestVariant
-getBestVariant knapsack (variant : allVariants) (bestValue, bestVariant) = do
+getBestVariant :: Knapsack -> [[Int]] -> (Int, Int, [Int])
+getBestVariant _ [] = (-1, -1, [])
+getBestVariant knapsack (variant : nextVariants) = do
     let (variantWeight, variantCost) = getVariantWeightAndCost (items knapsack) variant
-    if variantWeight > maxWeight knapsack || variantCost < bestValue
-        then getBestVariant knapsack allVariants (bestValue, bestVariant)
-        else getBestVariant knapsack allVariants (variantCost, variant)
+    let (bestWeight, bestValue, bestVariant) = getBestVariant knapsack nextVariants
+    if variantWeight >= maxWeight knapsack || variantCost < bestValue
+        then (bestWeight, bestValue, bestVariant)
+        else (variantWeight, variantCost, variant)
 
 -- Solve knapsack problem with brute force (trying all permutations of knapsack problem)
 solveKnapsackBruteforce :: Knapsack -> (Bool, Solution)
 solveKnapsackBruteforce knapsack
     | null (items knapsack) = (True, Solution [])
     | otherwise = do
-        (True, Solution array) `debug` show weight
-        where
-            (weight ,array) = getBestVariant knapsack (generatePermutations (length (items knapsack))) (-1, [])
-
+        if cost < minCost knapsack then
+            (False, Solution [])
+        else
+            (True, Solution array) `debug` (show weight ++ " " ++ show cost) where
+            (weight, cost, array) = getBestVariant knapsack (generatePermutations (length (items knapsack)))
